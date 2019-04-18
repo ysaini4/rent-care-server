79505e1afeb4f36c7aa0a3fad76e55a198761fcd
ysaini3 <yogesh.saini@myqsoft.com>
2019-04-18 14:41:56 +0530

    th

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-//const config = require("config");
+const config = require("config");
 const app = express();
 
-const port = process.env.PORT;
-console.log(process.env.env, app.get("env"));
+require("./startup/db")();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Yogy");
+});
 const server = app.listen(port, () => {
   console.log(`server is listening at ${port}`);
 });
