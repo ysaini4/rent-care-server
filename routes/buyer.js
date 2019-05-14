f8c35bd18eff2aead883c7db73dbcade61658de4
ysaini3 <yogesh.saini@myqsoft.com>
2019-05-14 16:14:48 +0530

    buyer

--- routes/buyer.js
+++ routes/buyer.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { Buyer } = require("../models/buyer");
+const { Property } = require("../models/property");
+
 router.post("/", async (req, res) => {
   try {
     req.body.Date = new Date();
@@ -13,6 +15,32 @@ router.post("/", async (req, res) => {
     res
       .status(500)
       .send({ status: false, msg: "Your details not submitted.", error: err });
+  }
+});
+router.post("/search", async (req, res) => {
+  try {
+    const buyer = await Buyer.find(req.body).lean();
+    let pIds = [];
+    let newBuyer = buyer.map(item => {
+      pIds.push(item.pId);
+      return {
+        BName: item.Name,
+        BMobile: item.Mobile,
+        BEmail: item.Email,
+        pId: item.pId
+      };
+    });
+    const properties = await Property.find({ _id: { $in: pIds } }).lean();
+    let newBuyerWithP = newBuyer.map(element => {
+      let p = properties.find(item => item._id.toString() === element.pId);
+
+      return (element = { ...element, ...p });
+    });
+    res.send(newBuyerWithP);
+  } catch (err) {
+    res
+      .status(500)
+      .send({ status: false, msg: "Unable to find Buyers", error: err });
   }
 });
 module.exports = router;
