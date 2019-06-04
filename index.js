7d5b4d8a0ef38cb2bcd603875896b63b25352b79
ysaini3 <yogesh.saini@myqsoft.com>
2019-06-04 15:28:42 +0530

    cluster

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+const cluster = require("cluster");
 const express = require("express");
 const app = express();
-
+const cpu = require("os").cpus();
+const numCPUs = cpu.length;
 require("./startup/db")();
 require("./startup/cloudnary")();
 require("./startup/routes")(app);
 
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-  console.log(`server is listening at ${port}`);
-});
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  console.log(numCPUs, "hh");
+  const port = process.env.PORT || 3000;
+  const server = app.listen(port, () => {
+    console.log(`server is listening at ${port}`);
+  });
+  console.log(`Worker ${process.pid} started`);
+}
